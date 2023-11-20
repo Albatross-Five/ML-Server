f3ad6b37d3ce27b8362d82f1e24ea2df983fbff4
Hong-Jupjup <100458240+Hong-Jupjup@users.noreply.github.com>
2023-11-20 21:52:59 +0900

    Update abnormal.py

--- abnormal.py
+++ abnormal.py
@@ -2,6 +2,21 @@ from fastapi import APIRouter, FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 import shutil
+import os
+from pathlib import Path
+from datetime import datetime
+
+from phone_detection import ObjectDetector
+from sleep_detection import DrowsinessDetector
+import imutils
+import cv2
+
+fps = 10
+result_phone = 0
+MAX_IMAGES = 20
+
+phone_detector = ObjectDetector()
+sleep_detector = DrowsinessDetector(0.25, 20)
 
 # 다른 서버의 API 엔드포인트
 other_server_api_url = "/abnormal/detect"
@@ -15,31 +30,52 @@ router = APIRouter(
 async def get_image(
     file: UploadFile = File(...)
 ):
-    
     try:
         # 멀티파트 데이터를 FormData로 변환
         data = {
-            "file": ("file.jpg", file.file)
+            "file": (file.filename, file.file)
         }
         
         # 파일을 임시 디렉토리에 저장
         temp_dir = "/home/hagima-ml/ML-Server/AbnormalImgs"
         image_paths = []
-        for key, (filename, file) in data.items():
-            file_path = f"{temp_dir}/{filename}"
-            with open(file_path, "wb") as dest:
-                shutil.copyfileobj(file, dest)
-            image_paths.append(file_path)
-
-        # 얼굴 인식 결과에 따라 응답 처리
-        """ if deep_result in ("-1", "0", "1", "2", "3"):
-            response_data = {"status": 200, "message": "얼굴 식별에 성공하였습니다.", "data": {"index": int(deep_result)}}
-        else:
-            response_data = {"status": 200, "message": "얼굴 식별에 실패하였습니다.", "data": {"index": -1}} """
+        
+        image_count = len(os.listdir(temp_dir))
+        
+        if image_count >= MAX_IMAGES:
+            oldest_file = min(Path(temp_dir).iterdir(), key = os.path.getctime)
+            os.remove(oldest_file)
             
-        response_data = {"status": 200, "message": "사진 옴.", "data": {"index": 1}}
+        # timestamp를 활용하여 파일 저장
+        timestamp = datetime.now().strftime("%y%m%d%H%M%S%f")[:-3]
+        filename = f"{timestamp}.jpg"
+
+        file_path = f"{temp_dir}/{filename}"
+        
+        with open(file_path, "wb") as dest:
+            shutil.copyfileobj(file.file, dest)
+        image_paths.append(file_path)
+
+        ######################################민석수정#########################################   
+
+        frame = cv2.imread(file_path, cv2.IMREAD_COLOR)
+        #frame = imutils.resize(frame, width = 416)
+        frame = imutils.resize(frame, width = 1024)
+
+        result_phone = phone_detector.detect(frame)
+        result_sleep = sleep_detector.detect_drowsiness(frame)
+        print("driver sleeping: {}, driver phoning: {}".format(result_sleep, result_phone))
+
+        ######################################################################################
+
+        #process_image(file_path)
+
+        # 얼굴 인식 결과에 따라 응답 처리            
+        response_data = {"status": 200, "message": "사진 옴", "sleep": result_sleep, "phone": result_phone}
             
         return JSONResponse(content=response_data, status_code=200)
+    
     except Exception as e:
         # 예외 처리: 에러 발생 시
-        return JSONResponse(content={"error": f"Error: {str(e)}"}, status_code=500)
+        response_data = {"status": 500, "message": "사진 안 옴"}
+        return JSONResponse(content=response_data, status_code=500)
